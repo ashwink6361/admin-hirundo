910abd205939c19815b0f13b0d7a943d96e3514b
unknown <v.shukla@huulke.com>
2018-04-05 15:45:25 +0530

    update

---
+++ src/app/pages/categories/tabs.Directive.js
@@ -0,0 +1,17 @@
+/**
+ * @author s.gautam@huulke.com
+ * created on 25.01.2018
+ */
+(function () {
+    'use strict';
+        angular.module('BlurAdmin.pages.categories').directive('menuTabs', menuTabs);
+        /** @ngInject */
+        function menuTabs() {
+            return {
+                restrict: 'EA',
+                controller: 'CategoryController',
+                templateUrl: 'app/pages/categories/views/tabs.html'
+            };
+        }
+    })();
+    
