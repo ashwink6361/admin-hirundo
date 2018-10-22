459878f48125d4e2e911de6efc2c556064a79251
Shivangi <s.gautam@huulke.com>
2018-10-22 17:47:05 +0530

    updates

--- src/app/pages/rooms/roomsController.js
+++ src/app/pages/rooms/roomsController.js
@@ -2074,7 +2074,7 @@
                 var WinPrint = window.open(data.data, '', 'left=0,top=0,width=800,height=900,toolbar=0,scrollbars=0,status=0');
                 // WinPrint.document.write(prtContent.innerHTML);
                 // WinPrint.document.close();
-                WinPrint.focus();            
+                // WinPrint.focus();            
                 // document.getElementById('invoicePrint').style.visibility = "hidden";
                 WinPrint.print();
                 // WinPrint.close();
