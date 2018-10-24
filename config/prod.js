e4369e9d9b922c4befdbb3067122bc795b825c8b
Shivangi <s.gautam@huulke.com>
2018-10-24 18:00:02 +0530

    updates

--- config/prod.js
+++ config/prod.js
@@ -5,7 +5,8 @@
 const path = require('path');
 const projectName = 'MVP Hirundo';
 const port = 5050;
-const apiUrl = 'http://52.209.187.183:5051';
+// const apiUrl = 'http://52.209.187.183:5051';
+const apiUrl = 'http://18.202.197.218:5051';
 
 module.exports = {
     env: 'production',
